8196387681c327460049a4e418c2012b9d2be527
ChristinaMartin13 <christina13martin@gmail>
2023-07-16 16:25:31 -0500

    Completed Part2 and Part 3

---
+++ part1/exercises/src/components/HobbyLinks.jsx
@@ -0,0 +1,11 @@
+
+export default function HobbyLinks() {
+      let hobbyLinks = ["https://m.media-amazon.com/images/I/51PNUlzZqlL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg","https://www.slua.org/"];
+      
+        return (
+          <div>HobbyLinks
+          <a href = {hobbyLinks[0]}>Swimming</a>
+          <a href = {hobbyLinks[1]}>Ultimate Frisbee</a>
+          </div>
+        )
+      }
