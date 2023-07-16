ca2f61e235bed3a76b0134f17f5210d1b04be68e
ChristinaMartin13 <christina13martin@gmail>
2023-07-16 15:52:05 -0500

    Completed part 1 of React Exercises 1

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Must Reads";
+   let book1 = "https://m.media-amazon.com/images/I/51p1WxfBJAL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/51wZ8r6BaJL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg";   
+   let book3 = "https://m.media-amazon.com/images/I/51PNUlzZqlL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg";
 
    return (
       <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
+      <h3>{pageTitle}</h3>
+      <img src={book1} alt="The Chronicles of Narnia" />
+      <img src={book2} alt="Gone Series" />
+      <img src={book3} alt="A Series of Unfortunate Events" />
+   </div>      
    );
 }
